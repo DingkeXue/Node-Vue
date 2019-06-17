011827fbedd470519ad0ec7019342a43f66b955f
DingkeXue <605534423@qq.com>
2019-06-18 01:17:33 +0800

    处理跨域请求

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ mongoose
   .catch(err => console.log(err));
 
 // 使用中间件实现允许跨域
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  next();
+});
 
 // passport 初始化
 app.use(passport.initialize());
